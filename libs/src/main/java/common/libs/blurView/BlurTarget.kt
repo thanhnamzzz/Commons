65cdda0fe50a6f3b3fe5b2a4c51bb8fd10168030
Nguyễn Thành Nam <ksthanhnam1995@gmail.com>
2026-01-30 08:57:21 +0700

    fix blur target

--- libs/src/main/java/common/libs/blurView/BlurTarget.kt
+++ libs/src/main/java/common/libs/blurView/BlurTarget.kt
@@ -15,36 +15,41 @@ import androidx.annotation.ChecksSdkIntAtLeast
  * This snapshot is used by the BlurView to apply blur effect.
  */
 class BlurTarget @JvmOverloads constructor(
-    context: Context,
-    attrs: AttributeSet? = null,
-    defStyleAttr: Int = 0,
-    defStyleRes: Int = 0,
+	context: Context,
+	attrs: AttributeSet? = null,
+	defStyleAttr: Int = 0,
+	defStyleRes: Int = 0,
 ) : FrameLayout(context, attrs, defStyleAttr, defStyleRes) {
-    @JvmField
-    var renderNode: RenderNode? = null
+	@JvmField
+	var renderNode: RenderNode? = null
 
-    init {
-        if (canUseHardwareRendering) {
-            renderNode = RenderNode("BlurViewHost node")
-        }
-    }
+	init {
+		if (canUseHardwareRendering) {
+			renderNode = RenderNode("BlurViewHost node")
+		}
+	}
 
-    override fun dispatchDraw(canvas: Canvas) {
-        if (canUseHardwareRendering && canvas.isHardwareAccelerated) {
-            renderNode!!.setPosition(0, 0, width, height)
-            val recordingCanvas = renderNode!!.beginRecording()
-            super.dispatchDraw(recordingCanvas)
-            renderNode!!.endRecording()
-            canvas.drawRenderNode(renderNode!!)
-        } else {
-            super.dispatchDraw(canvas)
-        }
-    }
+	override fun dispatchDraw(canvas: Canvas) {
+		if (canUseHardwareRendering && canvas.isHardwareAccelerated) {
+			renderNode!!.setPosition(0, 0, width, height)
+			val recordingCanvas = renderNode!!.beginRecording()
+			super.dispatchDraw(recordingCanvas)
+			renderNode!!.endRecording()
+			canvas.drawRenderNode(renderNode!!)
+//		} else {
+//			try {
+//				super.dispatchDraw(canvas)
+//			} catch (e: Exception) {
+//				Log.e("Namzzz", "BlurTarget: dispatchDraw", e)
+//				return
+//			}
+		}
+	}
 
-    companion object {
-        // Need both RenderNode (API 29) and RenderEffect (API 31) to be available for a full hardware rendering pipeline
-        @ChecksSdkIntAtLeast(api = Build.VERSION_CODES.S)
-        @JvmField
-        val canUseHardwareRendering: Boolean = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
-    }
+	companion object {
+		// Need both RenderNode (API 29) and RenderEffect (API 31) to be available for a full hardware rendering pipeline
+		@ChecksSdkIntAtLeast(api = Build.VERSION_CODES.S)
+		@JvmField
+		val canUseHardwareRendering: Boolean = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
+	}
 }
