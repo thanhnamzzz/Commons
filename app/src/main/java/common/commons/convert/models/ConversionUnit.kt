c807046f9bf0ed35a6b38077d4eeaefa91a4cbab
Nguyễn Thành Nam <ksthanhnam1995@gmail.com>
2026-04-15 10:37:08 +0700

    convert

--- app/src/main/java/common/commons/convert/models/ConversionUnit.kt
+++ app/src/main/java/common/commons/convert/models/ConversionUnit.kt
@@ -7,11 +7,7 @@ data class ConversionUnit(
     val name: String,
     val factor: Double,
     val unitSymbol: String,
-) {
-    companion object {
-        val EMPTY = ConversionUnit("", 1.0, "")
-    }
-}
+)
 
 val MC = MathContext(9, RoundingMode.HALF_UP)
 const val SQUARE_POSTFIX = "²"
