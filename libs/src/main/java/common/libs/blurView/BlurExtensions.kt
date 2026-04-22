35189342034d4235fac2443da9fe7ef57ac05639
Nguyễn Thành Nam <ksthanhnam1995@gmail.com>
2026-04-22 17:20:47 +0700

    fix blurView

--- libs/src/main/java/common/libs/blurView/BlurExtensions.kt
+++ libs/src/main/java/common/libs/blurView/BlurExtensions.kt
@@ -1,6 +1,7 @@
 package common.libs.blurView
 
 import android.graphics.Outline
+import android.util.Log
 import android.view.View
 import android.view.ViewOutlineProvider
 import android.view.Window
@@ -9,22 +10,30 @@ fun Window.setupBlurBackground(
 	viewBlur: BlurView,
 	viewTarget: BlurTarget,
 	radius: Float = 10f,
-	withBackground: Boolean = true
+	withBackground: Boolean = true,
+	setupBlurFail: () -> Unit = {}
 ) {
-	viewBlur.apply {
-		if (withBackground) {
-			clipToOutline = true
-			outlineProvider = object : ViewOutlineProvider() {
-				override fun getOutline(p0: View?, p1: Outline?) {
-					p1?.let {
-						viewBlur.background.getOutline(it)
-						it.alpha = 1f
+	viewBlur.post {
+		try {
+			viewBlur.apply {
+				if (withBackground) {
+					clipToOutline = true
+					outlineProvider = object : ViewOutlineProvider() {
+						override fun getOutline(p0: View?, p1: Outline?) {
+							p1?.let {
+								viewBlur.background.getOutline(it)
+								it.alpha = 1f
+							}
+						}
 					}
 				}
+				setupWith(viewTarget)
+					.setFrameClearDrawable(decorView.background)
+					.setBlurRadius(radius)
 			}
+		} catch (e: Exception) {
+			Log.e("Namzzz", "BlurView: setupBlurBackground ERROR:", e)
+			setupBlurFail()
 		}
-		setupWith(viewTarget)
-			.setFrameClearDrawable(decorView.background)
-			.setBlurRadius(radius)
 	}
 }
