e4d37d240807abb601d806725e5a610f7c595ceb
Nguyễn Thành Nam <ksthanhnam1995@gmail.com>
2026-04-24 09:42:30 +0700

    update function sort

--- libs/src/main/java/common/libs/functions/Sort.kt
+++ libs/src/main/java/common/libs/functions/Sort.kt
@@ -1,13 +1,30 @@
 package common.libs.functions
 
-fun <T, R : Comparable<R>> sortByProperty(
-    list: Iterable<T>,
-    isAscending: Boolean = true,
-    propertySelector: (T) -> R,
+/**
+ * Sắp xếp một Iterable dựa trên một thuộc tính.
+ *
+ * @param isAscending true nếu sắp xếp tăng dần, false nếu giảm dần.
+ * @param propertySelector Hàm chọn thuộc tính để sắp xếp.
+ */
+inline fun <T, R : Comparable<R>> sortByProperty(
+	list: Iterable<T>,
+	isAscending: Boolean = true,
+	crossinline propertySelector: (T) -> R,
 ): List<T> {
     return if (isAscending) {
         list.sortedWith(compareBy { propertySelector(it) })
     } else {
         list.sortedWith(compareByDescending { propertySelector(it) })
     }
+}
+
+inline fun <T, R : Comparable<R>> Iterable<T>.sortByProperty(
+	isAscending: Boolean = true,
+	crossinline propertySelector: (T) -> R
+): List<T> {
+	return if (isAscending) {
+		this.sortedBy(propertySelector)
+	} else {
+		this.sortedByDescending(propertySelector)
+	}
 }
